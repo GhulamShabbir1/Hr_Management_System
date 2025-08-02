1b18dc709e89cef6c486dcbe691cc5948a94614a
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-08-02 17:08:22 +0500

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Custom CSS with gradients
+import "./assets/css/custom.css";
+
 Vue.config.productionTip = false;
+
+// Initialize authentication state from localStorage
+store.dispatch('auth/initializeAuth');
 
 new Vue({
   router,
